05ba5c0d64944bd3a75a23787dfc632ff96f5b99
compasses <gnhe2009@gmail.com>
2017-04-01 22:40:14 +0800

    add test

---
+++ scala-base/notes/functionalProgramming.md
@@ -0,0 +1,13 @@
+## Functional Programming and Scala
+
+因为想学习下Scala接触到了函数式编程。学习Scala更多的是一种思想的转变。不同于传统的编程语言，例如C++，Java，Golang等，个人感觉他们没有本质的区别，无非是内存、CPU、IO等等。所以我有三年的C++开放经验，不管是写Java还是Golang，没感觉到违和感。跳越不是很大。
+直到最近，想学习下大数据的语言Scala。学习Scala更多的是一种思想的转变，不是传统的面向『计算机的』开发了，而是面向函数式的。所以要比较好的掌握Scala，思想转变是必须的。
+
+[Functional Programming](https://en.wikipedia.org/wiki/Functional_programming)是个很大的topic，自己才疏学浅直到最近学习了Scala接触了下。它的理论基础是[Lambda calculus](https://en.wikipedia.org/wiki/Lambda_calculus)，Lambda calculus是一种数学计算抽象。研究生期间还上过这个计算理论课。理解基本上就是一些Lambda算子的各种变换和证明。所以函数式编程可以理解为面向函数的编程。就像Java一样，面向对象编程，而它的理论基础便是Lambda calculus，[这里](https://github.com/txyyss/Lambda-Calculus/releases)有对其比较好的介绍。
+
+
+
+
+
+
+
--- scala-base/src/main/scala/programming/impatient/test.scala
+++ scala-base/src/main/scala/programming/impatient/test.scala
@@ -1,5 +1,7 @@
 package programming.impatient
 
+import breeze.numerics.pow
+
 /**
   * Created by I311352 on 3/31/2017.
   */
@@ -30,5 +32,50 @@ object test extends App{
     x -= 1
     println(x)
   }
+
+
+  val names = List("pp")
+  val two = names :+ "ok"
+
+  println(two)
+
+  println(List(1, 7, 2, 9).reduceLeft(_ - _))
+  println(List(1, 7, 2, 9).reduceRight(_ - _))
+  println(List(1, 7, 2, 9).foldLeft(0)(_ - _))
+
+  val freq = scala.collection.mutable.Map[Char, Int]()
+
+  for( c <- "Mississippi") freq(c) = freq.getOrElse(c, 0) + 1
+  println(freq)
+
+  (Map[Char, Int]() /: "Mississippi") {
+    (m, c) => m + (c-> (m.getOrElse(c, 0) + 1))
+  }
+
+  println((1 to 10).scanLeft(0)(_+_))
+
+
+  val res = List(50.0, 40.0, 9.59) zip List(10, 2)
+  println(res.map {p => p._1 * p._2})
+
+  val res2 = List(50.0, 40.0, 9.59) zipAll(List(10, 2), 0.0, 1)
+  val powers = (0 until 1000).view.map(pow(10, _))
+
+  println(powers(999))
+
+  def indexes(str: String) : scala.collection.mutable.Map[Char, List[Int]] = {
+    var i = 0
+    val freq = scala.collection.mutable.Map[Char, List[Int]]()
+
+    for (c <- str) {var ll = freq.getOrElse(c, List()); ll = ll :+ i; freq(c) = ll; i+=1}
+    freq
+  }
+
+//  def indexes2(str: String) : Map[Char, List[Int]] = {
+//
+//    str map(ch =>
+//  }
+
+  println(indexes("Mississippi"))
 
 }
